8bb299143afd7ecdf5f61559689864de87629f90
phuchoa <phuchoa1202@gmail.com>
2024-03-26 22:25:18 +0700

    Hoa/them_ham_cloneObject

---
+++ src/utils/cloneObject.js
@@ -0,0 +1,16 @@
+function cloneObject(obj) {
+  if (obj == null || typeof obj !== 'object') {
+    return {};
+  }
+
+  const clone = {};
+  for (let i in obj) {
+    if (obj[i] != null && typeof(obj[i]) == "object")
+      clone[i] = cloneObject(obj[i]);
+    else
+      clone[i] = obj[i];
+  }
+  return clone;
+}
+
+module.exports = cloneObject;
---
+++ tests/cloneObject.test.js
@@ -0,0 +1,59 @@
+const cloneObject = require('../src/utils/cloneObject');
+
+describe('cloneObject Function', () => {
+  test('should clone a simple object', () => {
+    const obj = { name: 'John', age: 30 };
+    const clonedObj = cloneObject(obj);
+    expect(clonedObj).toEqual(obj);
+    expect(clonedObj).not.toBe(obj);
+  });
+
+  test('should clone a nested object', () => {
+    const obj = {
+      name: 'John',
+      address: {
+        city: 'New York',
+        country: 'USA'
+      }
+    };
+    const clonedObj = cloneObject(obj);
+    expect(clonedObj).toEqual(obj);
+    expect(clonedObj).not.toBe(obj);
+    expect(clonedObj.address).not.toBe(obj.address);
+  });
+
+  test('should handle null values', () => {
+    const obj = { name: null };
+    const clonedObj = cloneObject(obj);
+    expect(clonedObj).toEqual(obj);
+  });
+
+  test('should clone an object containing a function', () => {
+    const obj = {
+      name: 'John',
+      greet: function() { console.log(`Hello, my name is ${this.name}`); }
+    };
+    const clonedObj = cloneObject(obj);
+    expect(clonedObj).toEqual(obj);
+    expect(clonedObj.greet).toEqual(obj.greet);
+    expect(clonedObj).not.toBe(obj);
+  });
+
+  test('should clone a deeply nested object', () => {
+    const obj = {
+      level1: {
+        level2: {
+          level3: {
+            name: 'John'
+          }
+        }
+      }
+    };
+    const clonedObj = cloneObject(obj);
+    expect(clonedObj).toEqual(obj);
+    expect(clonedObj).not.toBe(obj);
+    expect(clonedObj.level1).not.toBe(obj.level1);
+    expect(clonedObj.level1.level2).not.toBe(obj.level1.level2);
+    expect(clonedObj.level1.level2.level3).not.toBe(obj.level1.level2.level3);
+  });
+});
