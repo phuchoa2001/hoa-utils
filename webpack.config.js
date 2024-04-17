f46d84a822d1bb1447282076b802b70ae47b8ce6
Đặng Phúc Hòa <95266292+phuchoa2001@users.noreply.github.com>
2024-04-17 19:24:25 +0700

    Hoa/automated mini app deployment to npm (#31)
    * Hoa/test
    
    * Hoa/test
    
    * Hoa/test
    
    * Hoa/test
    
    * Hoa/test
    
    * Hoa/test
    
    * Hoa/Automated Mini-App Deployment to npm

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'lib-mini', 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+  },
+  optimization: {
+    minimizer: [new TerserPlugin()],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './package.json',
+          to: path.resolve(__dirname, 'lib-mini'),
+          transform: (content) => {
+            const packageJson = JSON.parse(content.toString());
+            packageJson.name = 'hoa-utils-mini';
+            packageJson.main = 'dist/index.js';
+            packageJson.files = ['dist/**/*'];
+            return Buffer.from(JSON.stringify(packageJson));
+          },
+        },
+        {
+          from: './README.md',
+          to: path.resolve(__dirname, 'lib-mini'),
+        },
+      ],
+    }),
+  ],
+  externals: [nodeExternals()]
+};
